791a52c937e29aa3a60a3d1ebdea70212794279b
Dominik Januszewicz <Dominik.Januszewicz.PL@ieee.org>
2017-03-10 22:52:41 +0100

    Szkielet aplikacji

---
+++ digital-signature/src/main/java/pl/mkoi/project/DigitalSignatureApplication.java
@@ -0,0 +1,12 @@
+package pl.mkoi.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalSignatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalSignatureApplication.class, args);
+	}
+}
