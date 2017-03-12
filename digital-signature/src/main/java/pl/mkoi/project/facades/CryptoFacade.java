a2f37854a875a8541e1b3210cc3d8f3b8ceec1ac
Dominik Januszewicz <Dominik.Januszewicz.PL@ieee.org>
2017-03-12 23:55:15 +0100

    MKOI-3 Generator kluczy dla RSA

--- digital-signature/src/main/java/pl/mkoi/project/facades/CryptoFacade.java
+++ digital-signature/src/main/java/pl/mkoi/project/facades/CryptoFacade.java
@@ -7,6 +7,9 @@ import java.security.SecureRandom;
 
 @Component
 public class CryptoFacade {
+
+  private static final int CERTAINTY = 4;
+
   /**
    * 
    * @param bitLength - size of number
@@ -32,53 +35,85 @@ public class CryptoFacade {
    * @param q second prime number
    * @return number of relatively prime numbers
    */
-
   public BigInteger eulerFunction(BigInteger primeP, BigInteger primeQ) {
     return primeP.subtract(BigInteger.ONE).multiply(primeQ.subtract(BigInteger.ONE));
   }
-  
+
   /**
-   * Generates a generator G which is needed in the DSA algorithm. 
+   * Generates a generator G which is needed in the DSA algorithm.
+   * 
    * @param p first prime number
    * @param q second prime number
    * @return generator G
    */
-  public BigInteger generateG(BigInteger p, BigInteger q)
-  {
-      BigInteger e = (p.subtract(BigInteger.ONE)).divide(q);
-      BigInteger g = BigInteger.ONE;
-      SecureRandom rand = new SecureRandom();
-      BigInteger h=new BigInteger(p.subtract(BigInteger.ONE).bitLength(), rand);
-      while(!g.equals(BigInteger.ONE))
-      {
-          g=h.modPow(e, p);
-      }
-      return g;
+  public BigInteger generateG(BigInteger p, BigInteger q) {
+    BigInteger e = (p.subtract(BigInteger.ONE)).divide(q);
+    BigInteger g = BigInteger.ONE;
+    SecureRandom rand = new SecureRandom();
+    BigInteger h = new BigInteger(p.subtract(BigInteger.ONE).bitLength(), rand);
+    while (!g.equals(BigInteger.ONE)) {
+      g = h.modPow(e, p);
+    }
+    return g;
   }
-  
+
   /**
    * Counts DSA signature
+   * 
    * @param primeP first number prime
    * @param primeQ second number prime
    * @param privateKey private secret key
    * @param hash hash of message to signed
    * @return signature
+   * 
    */
-  private byte[] countSignatureDSA(BigInteger primeP, BigInteger primeQ, BigInteger privateKey, BigInteger hash)
-  {
+  @SuppressWarnings("UPM_UNCALLED_PRIVATE_METHOD")
+  private byte[] countSignatureDSA(BigInteger primeP, BigInteger primeQ, BigInteger privateKey,
+      BigInteger hash) {
     SecureRandom rand = new SecureRandom();
 
     BigInteger secretNumberK = new BigInteger(primeQ.bitLength(), rand);
     BigInteger invertedK_1 = secretNumberK.modInverse(primeQ);
-    
-    BigInteger generatorG = generateG(primeP,primeQ);
+
+    BigInteger generatorG = generateG(primeP, primeQ);
     BigInteger r = generatorG.modPow(secretNumberK, primeP);
-    r=r.mod(primeQ);  
-    BigInteger s = (invertedK_1.multiply(hash.add(privateKey.multiply(r)))).mod(primeQ); 
-    
+    r = r.mod(primeQ);
+    BigInteger s = (invertedK_1.multiply(hash.add(privateKey.multiply(r)))).mod(primeQ);
+
     byte[] Signature = s.toByteArray();
-   
+
     return Signature;
+  }
+
+  /**
+   * Generates public and private key pair.
+   * @param publicKey public key
+   * @param privateKey private key
+   * @param bitLength keys length in bits
+   */
+  public void generateKeys(Key publicKey, Key privateKey, int bitLength) {
+    BigInteger primeP = getPrimeNumber(bitLength / 2, CERTAINTY);
+    BigInteger primeQ = getPrimeNumber(bitLength / 2, CERTAINTY);
+    BigInteger phi = eulerFunction(primeP, primeQ);
+
+    // Gets coprime number from 0 < e < phi(p*q)
+    // Gets randomly prime number which may be coprime to phi, thus we need to do primality test. If
+    // number is not coprime, increments it and tests again
+    BigInteger primeE = getPrimeNumber(bitLength, 4);
+    while (phi.gcd(primeE).compareTo(BigInteger.ONE) > 0 && primeE.compareTo(phi) < 0) {
+      primeE = primeE.add(BigInteger.ONE);
+    }
+
+    BigInteger factorN = primeP.multiply(primeQ);
+
+    // Public key is (n,e)
+    publicKey.setModulus(factorN);
+    publicKey.setExponent(primeE);
     
+    // Solves d*e = 1*(mod phi)
+    BigInteger factorD = primeE.modInverse(phi);
+    // Private key is (n,d)
+    privateKey.setModulus(factorN);
+    privateKey.setExponent(factorD);
   }
 }
--- digital-signature/src/main/java/pl/mkoi/project/services/impl/RsapssAlgorithmService.java
+++ digital-signature/src/main/java/pl/mkoi/project/services/impl/RsapssAlgorithmService.java
@@ -4,9 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import pl.mkoi.project.facades.CryptoFacade;
+import pl.mkoi.project.facades.Key;
 import pl.mkoi.project.services.SignatureAlgorithmService;
+
 
 @Component
 public class RsapssAlgorithmService implements SignatureAlgorithmService {
@@ -21,8 +22,13 @@ public class RsapssAlgorithmService implements SignatureAlgorithmService {
 
   @Override
   public String signFile(byte[] file) {
-    LOGGER.info("Liczba liczb względnie pierwszych: {}", cryptoUtils
-        .eulerFunction(cryptoUtils.getPrimeNumber(512, 4), cryptoUtils.getPrimeNumber(512, 4)));
+    Key pub = new Key();
+    Key priv = new Key();
+
+    cryptoUtils.generateKeys(pub, priv, 1024);
+    LOGGER.info("Pub key :{} {}", pub.getExponent(), pub.getModulus());
+    LOGGER.info("Priv key :{} {}", priv.getExponent(), priv.getModulus());
+    
     return null;
   }
 
